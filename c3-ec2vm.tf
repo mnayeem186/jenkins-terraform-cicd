6adeaf3bdc1e8a7f4a3f8ab79c3b67322a7c59c9
mnayeem <mnayeem186@gmail.com>
2022-06-26 16:48:00 +0530

    cicd poll scm test

--- c3-ec2vm.tf
+++ c3-ec2vm.tf
@@ -3,5 +3,8 @@ resource "aws_instance" "ec2vm" {
   ami = "ami-068257025f72f470d"
   instance_type = var.aws_instance_type
   user_data = file("${path.module}/jenkins-setup.sh")
+  tags = {
+    Name = "Terraform-cicd-Demo"
+  }
 
   }
